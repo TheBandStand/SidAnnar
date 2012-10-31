5522907f3c86dc731e1d91af62163739e98f8af9
steinit <steinitoddu@gmail.com>
2012-10-31 18:32:48 +0000

    Hello world commit

--- SidannarIjProject/src/HelloWorld.java
+++ SidannarIjProject/src/HelloWorld.java
@@ -1,7 +1,9 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
+        Steini steini = new Steini();
         System.out.println("Hello, World");
+        System.out.println(steini.hello);
     }
 
 }
---
+++ SidannarIjProject/src/Steini.java
@@ -0,0 +1,10 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Lenovo
+ * Date: 31.10.2012
+ * Time: 18:29
+ * To change this template use File | Settings | File Templates.
+ */
+public class Steini {
+    String hello = "Steini segir líka hæ :-)";
+}
